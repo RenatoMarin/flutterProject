8add8ad04c9d3561b5a3cb452777547e15390ef0
Renato.m.catto <renato.mcatto@gmail.com>
2020-09-01 13:17:39 -0400

    Adição das Notas, Nova nav e package

---
+++ lib/models/note_for_listing.dart
@@ -0,0 +1,12 @@
+class NoteForlisting {
+  String noteID;
+  String noteTitle;
+  DateTime createDateTime;
+  DateTime latesEditDateTime;
+
+  NoteForlisting(
+      {this.noteID,
+      this.noteTitle,
+      this.createDateTime,
+      this.latesEditDateTime});
+}
--- lib/views/note_list.dart
+++ lib/views/note_list.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterapi/models/note_for_listing.dart';
+import 'package:flutterapi/views/note_modify.dart';
 
 class NoteList extends StatelessWidget {
+  final notes = [
+    new NoteForlisting(
+        noteID: "1",
+        createDateTime: DateTime.now(),
+        latesEditDateTime: DateTime.now(),
+        noteTitle: "Nota 1"),
+    new NoteForlisting(
+        noteID: "1",
+        createDateTime: DateTime.now(),
+        latesEditDateTime: DateTime.now(),
+        noteTitle: "Nota 2"),
+    new NoteForlisting(
+        noteID: "1",
+        createDateTime: DateTime.now(),
+        latesEditDateTime: DateTime.now(),
+        noteTitle: "Nota 3"),
+  ];
+
+  //Formatar a data para DD/MM/AAAA
+  String formatDateTime(DateTime dateTime) {
+    return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Lista de recados')),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (_) => NoteModify()));
+        },
         child: Icon(Icons.add),
       ),
       body: ListView.separated(
@@ -15,15 +43,16 @@ class NoteList extends StatelessWidget {
         itemBuilder: (_, index) {
           return ListTile(
             title: Text(
-              'Hello',
+              notes[index].noteTitle,
               style: TextStyle(
                   color: Theme.of(context)
                       .primaryColor), //Se for trocado a cor primaria, será trocado a primaryColor também
             ),
-            subtitle: Text('Editado em 21/2/2020'),
+            subtitle: Text(
+                'Editado em ${formatDateTime(notes[index].latesEditDateTime)}'),
           );
         },
-        itemCount: 30,
+        itemCount: notes.length,
       ),
     );
   }
